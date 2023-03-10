81e12c5412c24f21b6ffc1a2eef86c1c573475bc
Dima L <dimitr8989@mail.ru>
2023-03-10 10:49:25 +0300

    Задача hotfix/17507 Карточка товара. Доработка кнопки Свернуть-Развернуть.

--- components/instruction-use/instruction-use.js
+++ components/instruction-use/instruction-use.js
@@ -10,9 +10,14 @@ function initInstructionUse() {
 	var content = container.querySelector('[data-instruction-use-content]')
 
 	input.addEventListener('change', function () {
-		if (input.checked) {
+		if (input.checked)
+		{
+			container.classList.add('is-open');
 			show(content, 500)
-		} else {
+		}
+		else
+		{
+			container.classList.remove('is-open');
 			hide(content, 500)
 		}
 	})
